7d4dcf2321aa9be56085269840df80883123a7ea
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-04-28 14:21:51 +0900

    콘솔기본  출력하기

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -10,6 +10,32 @@ namespace CSBasic
     {
         static void Main(string[] args)
         {
+            //273;
+            var name = "유" + "진" + "희";
+            Console.Write("Hello"+name+"\n");//출력
+            //int if= 2; 특정한 구문을 변수로 사용 X
+
+            Console.WriteLine("Hello World");
+            Console.Write("Hello World ");
+            Console.Write("Hello World ");
+
+            Console.Write(52);
+            Console.WriteLine(7 % 3);
+            Console.WriteLine('A');
+            Console.WriteLine("A");
+
+            Console.WriteLine("한빛\t아카데미");
+            Console.WriteLine("한빛\n아카데미");
+            Console.WriteLine("\"\"\"");
+
+            //Console.WriteLine("안녕"[100]);
+            Console.WriteLine("안녕"[0]);
+            Console.WriteLine("안녕"[1]);
+
+            Console.WriteLine(true);
+            Console.WriteLine(!true);
+
+
         }
     }
 }
